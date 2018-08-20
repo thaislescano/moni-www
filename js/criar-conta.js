efc822cf91aef1fde6cd0097dbbe62d47bbcd3f8
Thaís Lescano <thaislescano@hotmail.com>
2018-08-20 20:10:32 -0300

    next: validação cadastro

--- js/criar-conta.js
+++ js/criar-conta.js
@@ -1,14 +1,22 @@
-function mostrarSnack(){
-	var snack = document.getElementById("snackbar");
+var Moni = Moni || {};
+Moni.criarConta = {
+	mostrarSnack: function (){
+		var snack = document.getElementById("snackbar");
 
-	snack.className = "show";
-	setTimeout(function(){ snack.className = snack.className.replace("show", ""); }, 3000);
+		snack.className = "show";
+		setTimeout(function(){ snack.className = snack.className.replace("show", ""); }, 3000);
+
+		var select1 = document.getElementsByTagName('select')[0];
+		var select2 = document.getElementsByTagName('select')[1];
 
 
-	document.getElementsByClassName("input-box")[0].value = '';
-	document.getElementsByClassName("input-box")[1].value = '';
-	document.getElementsByClassName("input-box")[2].value = '';
-	document.getElementsByClassName("input-box")[3].value = '';
+		document.getElementsByClassName("input-box")[0].value = '';
+		document.getElementsByClassName("input-box")[1].value = '';
+		select1.selectedIndex = 0;
+		select2.selectedIndex = 0;
 
-	document.getElementsByClassName("input-check")[0].checked = false;
+		document.getElementsByClassName("input-check")[0].checked = false;
+
+	},
+
 }
