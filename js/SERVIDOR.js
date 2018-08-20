a2b845b1a4dc82b1a98202198f8df0cbe5db1ef3
Thaís Lescano <thaislescano@hotmail.com>
2018-08-19 21:48:17 -0300

    tela cadastro - gif maluco

--- js/SERVIDOR.js
+++ js/SERVIDOR.js
@@ -1,19 +1,31 @@
-var SERVIDOR = SERVIDOR || {
-	chamadaGet: function(parametros, callback){
-		let xhr = new XMLHttpRequest();
-		parametros = "http://192.168.0.103:8010/" + JSON.stringify(parametros);
-		xhr.onload = function() {
-			callback(xhr.response);
-		}
-		xhr.open("GET", parametros);
-		xhr.send();
-	}
-}
-var EXEMPLO = { //EXEMPLO DE PARAMETROS
-	tipo: "cardMonitor",
-	materia:"quimica"
-}
+// var SERVIDOR = SERVIDOR || {
+// 	chamadaGet: function(parametros, callback){
+// 		let xhr = new XMLHttpRequest();
+// 		parametros = "http://192.168.0.103:8010/" + JSON.stringify(parametros);
+// 		xhr.onload = function() {
+// 			callback(xhr.response);
+// 		}
+// 		xhr.open("GET", parametros);
+// 		xhr.send();
+// 	}
+// }
+// var EXEMPLO = { //EXEMPLO DE PARAMETROS
+// 	tipo: "cardMonitor",
+// 	materia:"quimica"
+// }
 
-SERVIDOR.chamadaGet(EXEMPLO, function(resposta){
-	alert(resposta);
-});
+// SERVIDOR.chamadaGet(EXEMPLO, function(resposta){
+// 	alert(resposta);
+// });
+
+
+
+// var EXEMPLO = { //EXEMPLO DE PARAMETROS
+// 	tipo: "login",
+// 	user:"quimica",
+// 	senha: "fgfdifui"
+// }
+
+// SERVIDOR.chamadaGet(EXEMPLO, function(resposta){
+// 	alert(resposta);
+// });
